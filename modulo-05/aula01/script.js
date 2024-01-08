97a04f4444a5394dad27d2e320ec2410caeee9e9
Jefferson Leandro Ramos de Oliveira <34520708+wasabibr@users.noreply.github.com>
2024-01-07 22:27:48 -0300

    Inserindo módulo 5

---
+++ modulo-05/aula01/script.js
@@ -0,0 +1,12 @@
+const mensagemDeBoasVindas1 = 'Bem vindo, torcedor Tricolor!';
+const mensagemDeBoasVindas2 = 'Bem vindo, torcedor Rubro-Negro!';
+
+console.log(mensagemDeBoasVindas1);
+console.log(mensagemDeBoasVindas2);
+
+const mensagemDeEscolha =
+  'Clique no setor para o qual deseja comprar seu ingresso!';
+const mensagemFinal = 'Divirta-se no Maracanã!!!';
+
+console.log(mensagemDeEscolha);
+console.log(mensagemFinal);
